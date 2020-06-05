357fb78a8f8cfc16d8e088bb910842c0bb59244c
João Marcos <jmoalmeida@gmail.com>
2020-06-05 11:37:56 -0300

    payment, one-to-one association

--- src/main/java/com/joaomarcos/curso/config/TestConfig.java
+++ src/main/java/com/joaomarcos/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.joaomarcos.curso.entities.Category;
 import com.joaomarcos.curso.entities.Order;
 import com.joaomarcos.curso.entities.OrderItem;
+import com.joaomarcos.curso.entities.Payment;
 import com.joaomarcos.curso.entities.Product;
 import com.joaomarcos.curso.entities.User;
 import com.joaomarcos.curso.enums.OrderStatus;
@@ -77,6 +78,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
 		
 	}
 
